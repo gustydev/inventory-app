fae55299343b71a330e710b44450244ae3fd0feb
gusty <gustavodiorlando@gmail.com>
2024-07-08 12:11:38 -0300

    Setup mongodb and create item and category models

---
+++ models/item.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+    name: {type: String, required: true, min: 3, max: 50},
+    description: {type: String, min: 1, max: 150},
+    price: {type: Number, required: true, min: 0},
+    stock: {type: Number, required: true, min: 0},
+    category: [{type: Schema.Types.ObjectId, ref: 'Category'}]
+})
+
+ItemSchema.virtual('url').get(function() {
+    return `inventory/item/${this._id}`;
+})
+
+module.exports = mongoose.model('Item', ItemSchema);
